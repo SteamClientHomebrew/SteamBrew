64c99c945906caa0b78117b832123aa9e144b600
Cecilia Sanare <ceci@sanare.dev>
2026-03-14 14:05:08 -0500

    fix: issue with middle clicking and ctrl clicking themes / plugins

--- apps/www/src/components/RenderFluenty.tsx
+++ apps/www/src/components/RenderFluenty.tsx
@@ -61,7 +61,7 @@ function DisplayFluentyAd() {
 	return (
 		<>
 			<a className="card-wrap" ref={cardRef} tabIndex={0} style={{ position: 'relative', userSelect: 'none' }}>
-				<div className="card" onClick={() => (window.location.href = '/fluenty-steam')} onContextMenu={handleContextMenu} style={{ width: '100%', height: '100%' }}>
+				<a className="card" href="/fluenty-steam" onContextMenu={handleContextMenu} style={{ width: '100%', height: '100%' }}>
 					<img loading="lazy" className="card-image" src="https://i.imgur.com/2aAaAES.gif" data-holder-rendered="true" />
 					<div className="card-body">
 						<h3 className="card-title">Fluenty</h3>
@@ -88,7 +88,7 @@ function DisplayFluentyAd() {
 						</svg>
 						<span className="downloadTagText">{fluenty.downloads}</span>
 					</div>
-				</div>
+				</a>
 				{contextMenu.visible && (
 					<ul
 						className="custom-context-menu"
